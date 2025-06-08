4b874193a183c48988cd9c87be783f7eca8a7263
ui-adiii <beaheroinyourlife@gmail.com>
2025-06-08 23:24:45 +0530

    side bar

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { use, useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const location = useLocation();
 
-export default Dashboard
+  const [tab, settab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      settab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <div className="flex min-h-screen flex-col md:flex-row">
+      <div className="md:w-56">
+        <DashSidebar />
+      </div>
+      <div>{tab === "profile" && <DashProfile />}</div>
+    </div>
+  );
+};
+
+export default Dashboard;
