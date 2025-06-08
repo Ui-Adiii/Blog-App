6f3b22299a96640197f034873415653d21476976
ui-adiii <beaheroinyourlife@gmail.com>
2025-06-08 22:33:05 +0530

    make dashboard private

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
-import About from './pages/About'
-import Signin from './pages/Signin'
-import Signup from './pages/Signup'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
-import Header from './components/Header'
-import FooterCom from './components/FooterCom'
-import { ToastContainer } from 'react-toastify';
-
-
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import FooterCom from "./components/FooterCom";
+import { ToastContainer } from "react-toastify";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
-
   return (
     <BrowserRouter>
-      <ToastContainer/>
-      <Header/>
+      <ToastContainer />
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}  />
-        <Route path="/about" element={<About/>}  />
-        <Route path="/sign-in" element={<Signin/>}  />
-        <Route path="/sign-up" element={<Signup/>}  />
-        <Route path="/dashboard" element={<Dashboard/>}  />
-        <Route path="/projects" element={<Projects/>}  />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/projects" element={<Projects />} />
       </Routes>
-      <FooterCom/>
+      <FooterCom />
     </BrowserRouter>
   );
 };
