2c18f03ce37b7ca5124fbb622558e518a306247f
ui-adiii <beaheroinyourlife@gmail.com>
2025-06-10 21:58:51 +0530

    api route for getting posts

--- Backend/controllers/post.controller.js
+++ Backend/controllers/post.controller.js
@@ -57,5 +57,51 @@ const createPost = async (req, res) => {
   }
 };
 
+const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
 
-export { createPost };
+    const totalPosts = await Post.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    ); // new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+      success: true,
+      message: "getting all the posts",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { createPost, getPosts };
--- Backend/routes/post.route.js
+++ Backend/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import userAuth from '../middlewares/auth.middleware.js';
-import { createPost, } from "../controllers/post.controller.js";
+import { createPost, getPosts, } from "../controllers/post.controller.js";
 import upload from '../middlewares/multer.middleware.js';
 
 const router = express.Router();
 
-router.post("/create", userAuth, upload.single('image'),createPost);
+router.post("/create", userAuth, upload.single('image'), createPost);
+router.get('/getposts', getPosts);
 
 export default router;
