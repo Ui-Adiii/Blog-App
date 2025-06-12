b8af229f40b7c7335d7cb48c9b4e6321154c9cf5
ui-adiii <beaheroinyourlife@gmail.com>
2025-06-12 21:07:33 +0530

    post page functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -13,7 +13,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
-import PostPage from "./components/PostPage";
+import PostPage from "./pages/PostPage";
 
 const App = () => {
   return (
